68a3ca2634fd4214a3515a913531eb16fb85d3eb
nurudeen sodeinde <nurudeensodeinde@gmail.com>
2022-06-25 19:34:24 +0100

    calculator

---
+++ index.js
@@ -0,0 +1,11 @@
+let x= parseFloat(parseInt(prompt("enter the first number")));
+let y= parseFloat(parseInt(prompt("enter the second number")));
+
+let a= x+y;
+let b=x-y;
+let c=x*y;
+let d=x/y;
+alert("the sum of the given numbers is:"+a);
+alert("the difference of the given numbers is:"+b);
+alert("the product of the given numbers is:"+c);
+alert("the quotient of the given numbers is:"+d);
